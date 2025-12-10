177f69135999713290e3b910bb1fdf827ed5c9e8
samadali09 <155896690+samadali09@users.noreply.github.com>
2025-12-10 22:15:58 +0500

    Refactor docusaurus.config.ts by removing comments.
    Removed comments and cleaned up the configuration file.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,29 +3,23 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-
   title: 'AI Humanoid Robotics',
   tagline: 'Complete Detail related to Humanoid Robotics',
   favicon: 'img/favicon.ico',
 
-  // Production URL yahan daalein (filhal dummy hai)
   url: 'https://your-docusaurus-site.example.com',
-  // Base URL '/' rakhein agar root domain hai
   baseUrl: '/',
 
-  // GitHub Pages deployment config (optional but recommended)
   organizationName: 'VLA-Capstone', 
   projectName: 'humanoid-robot-docs', 
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
-  // Internationalization settings
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
   },
-  // -----------------------------------------
 
   presets: [
     [
@@ -42,12 +36,10 @@ const config: Config = {
   ],
 
   themeConfig: {
-    // Navbar Settings
     navbar: {
       title: 'AI Humanoid Robotics',
       logo: {
         alt: 'AI Book',
-        // Note: Make sure ye image 'static/img/logo.svg' mein maujood ho
         src: 'img/logo.svg', 
       },
       items: [
@@ -57,12 +49,8 @@ const config: Config = {
           position: 'left',
           label: 'AI Robotics Docs',
         },
-      
       ],
     },
-    
-
-    // Prism Code Block Theme
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
